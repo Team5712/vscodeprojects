1d2554ad0abde3494f304a91f8dad307c1bdab16
Team5712 <haltom@hemlockps.com>
2022-03-07 19:39:07 -0500

    Refractoring

--- MK4I Drive Only/src/main/java/frc/robot/RobotContainer.java
+++ MK4I Drive Only/src/main/java/frc/robot/RobotContainer.java
@@ -14,23 +14,15 @@ import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.Button;
-import frc.robot.commands.ClimbArmBack;
-import frc.robot.commands.ClimbArmDown;
-import frc.robot.commands.ClimbArmForward;
-import frc.robot.commands.ClimbArmUp;
-import frc.robot.commands.DefaultDriveCommand;
-import frc.robot.commands.PickUpBall;
-import frc.robot.commands.ReverseIntake;
-import frc.robot.commands.RunIntake;
-import frc.robot.commands.RunLowerMag;
-import frc.robot.commands.RunUpperMag;
-import frc.robot.commands.ShootHigh;
-import frc.robot.commands.ShootLow;
+
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Magazine;
 import frc.robot.subsystems.Shooter;
+import frc.robot.commands.*;
+import frc.robot.commands.Auto.RunBasicTrajectory;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -45,9 +37,11 @@ public class RobotContainer {
   private final Intake m_intake = new Intake();
   private final Magazine m_magazine = new Magazine();
   private final Climber m_climber = new Climber();
+  private final Limelight m_limelight = new Limelight();
   private final XboxController m_controller1 = new XboxController(0);
   private final XboxController m_controller2 = new XboxController(1);
   private final Compressor m_compressor = new Compressor(PneumaticsModuleType.REVPH);
+  private double visionCorrection;
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -58,14 +52,15 @@ public class RobotContainer {
     // Left stick Y axis -> forward and backwards movement
     // Left stick X axis -> left and right movement
     // Right stick X axis -> rotation
+
     m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
             m_drivetrainSubsystem,
             () -> -modifyAxis(m_controller1.getLeftY()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
             () -> -modifyAxis(m_controller1.getLeftX()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
-            () -> -modifyAxis(-m_controller1.getRightX()) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
+            () -> -modifyAxis(-m_controller1.getRightX() + m_limelight.turnToTarget()) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
     ));
 
-    
+    m_climber.runToPosition(5);
     m_compressor.enableAnalog(115, 120);
     // Configure the button bindings
     configureButtonBindings();
@@ -81,8 +76,8 @@ public class RobotContainer {
   private void configureButtonBindings() {
     // Back button zeros the gyroscope
     new Button(m_controller1::getAButton)
-            // No requirements because we don't need to interrupt anything
-            .whenPressed(m_drivetrainSubsystem::zeroGyroscope);
+           // No requirements because we don't need to interrupt anything
+           .whenPressed(m_drivetrainSubsystem::zeroGyroscope);
 
 
             // new Button(m_controller::getRightBumper)
@@ -112,37 +107,61 @@ public class RobotContainer {
   //             // No requirements because we don't need to interrupt anything
   //             .whenHeld(new RunLowerMag(m_magazine));
 
-  new Button(m_controller1::getStartButton)
-  .whenPressed(new RunLowerMag(m_magazine));
+  //new Button(m_controller1::getStartButton)
+  //.whenHeld(new IntakeDown(m_intake));
+
+  new Button(m_controller1::getXButton)
+    .whenHeld(new CalibrateHood(m_shooter));
 
   new Button(m_controller2::getRightBumper)
   // No requirements because we don't need to interrupt anything
-    .whenHeld(new ShootHigh(m_shooter));
+    .whenHeld(new Shoot(m_shooter, m_magazine));
   new Button(m_controller2::getLeftBumper)
     // No requirements because we don't need to interrupt anything
-      .whenHeld(new ShootLow(m_shooter));
+      .whenHeld(new RunLowerMag(m_magazine)
+      .alongWith(new RunUpperMag(m_magazine)));
 
   new Button(m_controller1::getRightBumper)
               // No requirements because we don't need to interrupt anything
-              .whenHeld(new PickUpBall(m_intake, m_magazine));
+              .whenHeld(new PickUpBall(m_intake, m_magazine)
+              .alongWith(new IntakeDown(m_intake)));
 
   new Button(m_controller1::getLeftBumper)
               // No requirements because we don't need to interrupt anything
               .whenHeld(new ReverseIntake(m_intake, m_magazine, m_shooter));
-
-  new Button(m_controller2::getStartButton)
+  
+  new Button(m_controller1::getYButton)
               // No requirements because we don't need to interrupt anything
-              .whenPressed(new ClimbArmUp(m_climber) //only go up 6 inches
-              .andThen(new ClimbArmForward(m_climber) //pneumatics out
-              .andThen(new ClimbArmUp(m_climber) // arms finish extending
-              .andThen(new ClimbArmBack(m_climber) // pneumatics in
-              .andThen(new ClimbArmDown(m_climber) // arms are retracted
-              .andThen(new ClimbArmUp(m_climber) //only go up 6 inches
-              .andThen(new ClimbArmForward(m_climber) //pneumatics out
-              .andThen(new ClimbArmUp(m_climber) // arms finish extending
-              .andThen(new ClimbArmBack(m_climber) // pneumatics in
-              .andThen(new ClimbArmDown(m_climber))))))))))); // arms are retracted
+              .whenHeld(new TurnToTarget(m_limelight));
+
+  // new Button(m_controller1::getStartButton)
+  //              // No requirements because we don't need to interrupt anything
+  //              .whenPressed(new ClimbArmDown(m_climber)
+  //              .andThen(new WaitCommand(1))
+  //              .andThen(new ClimbArmUp6in(m_climber) //only go up 6 inches
+  //              .andThen(new ClimbArmForward(m_climber) //pneumatics out
+  //              .andThen(new WaitCommand(1)) //wait 1 second
+  //              .andThen(new ClimbArmUp(m_climber) // arms finish extending
+  //              .andThen(new ClimbArmBack(m_climber) // pneumatics in
+  //              .andThen(new WaitCommand(1) //wait 1 second
+  //              .andThen(new ClimbArmDown(m_climber) // arms are retracted
+  //              .andThen(new WaitCommand(3))
+  //              .andThen(new ClimbArmUp6in(m_climber) //only go up 6 inches
+  //              .andThen(new ClimbArmForward(m_climber) //pneumatics out
+  //              .andThen(new WaitCommand(1) //wait 1 second
+  //              .andThen(new ClimbArmUp(m_climber) // arms finish extending
+  //              .andThen(new ClimbArmBack(m_climber) // pneumatics in
+  //              .andThen(new WaitCommand(1)) //wait 1 second
+  //              .andThen(new ClimbArmDown(m_climber)))))))))))))); // arms are retracted
+    
+  // new Button(m_controller1::getAButton)
+  //           .whenPressed(new ClimbArmUp(m_climber));
+  
+  // new Button(m_controller1::getBButton)
+  //           .whenPressed(new ClimbArmDown(m_climber));
+
   }
+
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -151,7 +170,8 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return new InstantCommand();
+    RunBasicTrajectory testauto = new RunBasicTrajectory(m_drivetrainSubsystem, "New Path"); //"New New New New New New New Path");
+    return testauto;
   }
 
   private static double deadband(double value, double deadband) {
