56e6b231b1bbb325e64e289396b4a576f05c7641
Team5712 <haltom@hemlockps.com>
2022-03-29 16:57:22 -0400

    more code cleanup post Midland

---
+++ MK4I Drive Only/src/main/java/frc/robot/commands/Auto/StraightPath.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Auto;
+
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.Constants;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.Shooter;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class StraightPath extends SequentialCommandGroup {
+  private Shooter m_shooter;
+  private DrivetrainSubsystem m_drivetrainSubsystem;
+  /** Creates a new StraightPath. */
+  public StraightPath(Shooter shooter, DrivetrainSubsystem drivetrainSubsystem) {
+    m_shooter = shooter;
+    m_drivetrainSubsystem = drivetrainSubsystem;
+    
+    m_shooter.zeroEncoderOfHood();
+    m_drivetrainSubsystem.zeroGyroscope();
+
+    TrajectoryConfig trajectoryConfig = Constants.auto.follower.T_CONFIG;
+
+    Trajectory trajectoryStraight = TrajectoryGenerator.generateTrajectory(
+        new Pose2d(-.7, 0, new Rotation2d(-10)),
+        List.of(
+            new Translation2d(-.2, .5),
+            new Translation2d(.1, .8)),
+        new Pose2d(.2, 1.7, Rotation2d.fromDegrees(-55)),
+        trajectoryConfig);
+
+        PIDController xController = Constants.auto.follower.X_PID_CONTROLLER;
+        PIDController yController = Constants.auto.follower.Y_PID_CONTROLLER;
+        ProfiledPIDController thetaController = Constants.auto.follower.ROT_PID_CONTROLLER;
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      new InstantCommand(() -> m_drivetrainSubsystem.resetOdometry(trajectoryStraight.getInitialPose())),
+      new SwerveControllerCommand(
+            trajectoryStraight,
+            m_drivetrainSubsystem::getPose2d,
+            m_drivetrainSubsystem.getKinematics(),
+            xController,
+            yController,
+            thetaController,
+            m_drivetrainSubsystem::setAllStates,
+            m_drivetrainSubsystem)
+    );
+  }
+}
---
+++ MK4I Drive Only/src/main/java/frc/robot/commands/PickUpBallNoIntake.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Magazine;
+
+public class PickUpBallNoIntake extends CommandBase {
+  /** Creates a new PickUpBall. */
+
+  private Magazine m_magazine;
+
+  public PickUpBallNoIntake(Magazine magazine) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_magazine = magazine;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    
+    // m_intake.moveIntake(-.4);
+    // m_magazine.runLowerMag(0.7);
+    // m_magazine.runUpperMag(-.7);
+    // high no ball is ~250, with ball ~1100
+    //System.out.println("High SENSOR VALUE:     " + m_magazine.getUpperBallSensor());
+    //System.out.println("LOWER BALL SENSOR VALUE:     " + m_magazine.getLowerBallSensor());
+    if (m_magazine.getUpperBallSensor() > Constants.UPPER_BALL_SENSOR_THRESHOLD) {  
+
+      if(m_magazine.getLowerBallSensor() < Constants.UPPER_BALL_SENSOR_THRESHOLD){
+      //System.out.println("full");
+      m_magazine.runLowerMag(0);
+      m_magazine.runUpperMag(0);
+      }
+      else{
+        //System.out.println("High ball in");
+        m_magazine.runLowerMag(0.45);
+        m_magazine.runUpperMag(0);
+      }
+    } 
+    else {
+      //System.out.println("Empty");
+      m_magazine.runLowerMag(0.45);
+      m_magazine.runUpperMag(-.2);
+    } 
+    
+    
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_magazine.runLowerMag(0);
+    m_magazine.runUpperMag(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+
+    return false;
+  }
+}
